a0c368fde6e3859a396809caa7349d47a419574b
Suman <suman1997dhull@gmail.com>
2023-02-25 15:07:51 +0530

    json

--- backend/Routes/user.routes.js
+++ backend/Routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const UserModel = require("../model/user.model");
+const UserModel = require("../Model/user.model");
 const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connection = require('./config/db');
+const connection = require('./Config/db');
 const userRoutes = require('./Routes/user.routes');
 dotenv.config();
 
